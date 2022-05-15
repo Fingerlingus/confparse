5d814e85ce4662ce37c4ba316ac16a66a572963a
Fingerlingus <sirfingerlingus@gmail.com>
2022-05-14 22:26:46 -0600

    add macros to correctly detect whether to use std::format or fmtlib, add constexpr ERROR() function to unify error detection between bool and integral values, add to_lower() for std::string_views, add debug-only log/error/print functions, add overloads to all log/error/print functions to accept a single value

--- src/util.hpp
+++ src/util.hpp
@@ -8,7 +8,22 @@
 #   define NO_DISCARD [[nodiscard]]
 #endif
 
-#ifdef _MSC_VER
+#if defined _MSC_VER
+#   define IS_MSVC
+#elif defined __GNU_C__
+#   define IS_GCC_OR_CLANG
+#endif
+
+#define CPP17 201703L
+#define CPP20 202002L
+
+#if __cplusplus > CPP17 && __cplusplus <= CPP20
+#   define IS_CPP20 1
+#elif __cplusplus > CPP20 && __cplusplus < 202300L
+#   define IS_CPP_LATEST 1
+#endif
+
+#if defined IS_MSVC && defined IS_CPP_LATEST
 namespace fmt = std;
 #include <format>
 #else
@@ -17,7 +32,6 @@ namespace fmt = std;
 #endif
 
 namespace util {
-
 // name prefixed with underscore due to overload resolution error with std::format() in MSVC
 template<typename ...Args> NO_DISCARD std::string _format(std::string_view fmt_str, Args&&... args) {
     return fmt::vformat(fmt_str, fmt::make_format_args(args...));
@@ -28,9 +42,19 @@ template<typename ...Args> void sprint(std::ostream& os, std::string_view fmt_st
     os << _format(fmt_str, args...);
 }
 
+// print single item to stream
+template<typename T> void sprint(std::ostream& os, const T& t) {
+    os << _format("{}", t);
+}
+
 // print formatted output to std::cout
 template<typename ...Args> void print(std::string_view fmt_str, Args&&... args) {
     sprint(std::cout, fmt_str, args...);
+}
+
+// print single item to std::cout
+template<typename T> void print(const T& t) {
+    sprint(std::cout, t);
 }
 
 // alias to sprint()
@@ -38,14 +62,72 @@ template<typename ...Args> void print(std::ostream& os, std::string_view fmt_str
     sprint(os, fmt_str, args...);
 }
 
+// alias to sprint()
+template<typename T> void print(std::ostream& os, const T& t) {
+    sprint(os, t);
+}
+
 // print formatted output to std::cout, with newline appended
 template<typename ...Args> void log(const std::string& fmt_str, Args&&... args) {
     print(fmt_str + '\n', args...);
 }
+
+// log single item
+template<typename T> void log(const T& t) {
+    log("{}", t);
+}
+#ifdef DEBUG
+// log if in debug mode
+template<typename ...Args> void dlog(const std::string& fmt_str, Args&&... args) {
+    log(fmt_str, args...);
+}
+
+template<typename T> void dlog(const T& t) {
+    log(t);
+}
+#else
+// else do nothing
+template<typename ...Args> void dlog(const std::string&, Args&&...) { }
+template<typename T> void dlog(const T&) { }
+#endif
 
 // print formatted output to std::cerr, with newline appended
 template<typename ...Args> void error(const std::string& fmt_str, Args&&... args) {
     print(std::cerr, fmt_str + '\n', args...);
 }
 
+// error with single item
+template<typename T> void error(const T& t) {
+    error("{}", t);
+}
+#ifdef DEBUG
+// error if in debug mode
+template<typename ...Args> void derror(const std::string& fmt_str, Args&&... args) {
+    error(fmt_str, args...);
+}
+
+template<typename T> void derror(const T& t) {
+    error(t);
+}
+#else
+// else do nothing
+template<typename ...Args> void derror(const std::string&, Args&&...) { }
+template<typename T> void derror(const T&) { }
+#endif
+
 } // namespace util
+
+NO_DISCARD constexpr bool ERROR(bool b) noexcept {
+    return b == false;
+}
+
+template<std::integral T> NO_DISCARD constexpr bool ERROR(T i) noexcept {
+    return i != 0;
+}
+
+NO_DISCARD constexpr std::string to_lower(std::string_view s) {
+    std::string l = "";
+    for (const auto& c : s)
+        l += static_cast<std::string::value_type>(std::tolower(c));
+    return l;
+}
